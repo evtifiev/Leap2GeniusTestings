32753eddc5e0e47fa2a925da94e68527d3a30316
Aleksey Evtifiev <evtifiev@gmail.com>
2019-05-16 14:00:33 +0300

    Update production name sever

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    API_URL: '"http://127.0.0.1:8000/api/"'
+    API_URL: '"http://api.leap2genius.com/api/"'
 })
