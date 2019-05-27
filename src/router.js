377e3f893d02efb037435fe1942c06c6734f8e67
Aleksey Evtifiev <evtifiev@gmail.com>
2019-05-27 16:59:33 +0300

    Delete static page. Add style file.

--- src/router.js
+++ src/router.js
@@ -1,10 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue'
 import Tests from './views/TestList.vue'
 import Passed from './views/Passed.vue'
-import About from './views/About.vue'
-import Contact from './views/Contact.vue'
 import Test from './views/Test.vue'
 import Login from './components/Login.vue'
 import Signup from './components/Signup.vue'
@@ -17,20 +14,6 @@ Vue.use(Router)
 let router = new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    }, {
-      path: '/contact',
-      name: 'contact',
-      component: Contact
-    },
     {
       path: '/test',
       name: 'test',
